e2d95026b32a48d6008aeb4be72e1728681d6555
LuigiVetere <215154651+Luigi-live@users.noreply.github.com>
2025-09-30 18:01:35 -0300

    feature: finalização exercicio 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let result = 0;
+    if(end < 0 || start < 0 || !Number.isInteger(start) || !Number.isInteger(end)){
+        return "ERROR"
+    }
+    else if (start > end){
+        while(end <= start){
+            result += end;
+            end ++;
+        }
+    } else{
+        while(start <= end){
+            result += start;
+            start ++;
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
