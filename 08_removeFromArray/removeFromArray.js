b0ca2004ff932e59c618d6f28fb888cce8b4635d
LuigiVetere <215154651+Luigi-live@users.noreply.github.com>
2025-09-30 17:44:53 -0300

    Feature: Finalização do exercicio 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...values) {
+    for(var value of values){
+        var i = 0;
+        var index = arr.indexOf(value);
+        if(index > -1){
+            while(i < arr.length){
+                if(arr[i] === value){
+                    arr.splice(i, 1);
+                } else {
+                    i++;
+                }
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
